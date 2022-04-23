e10ba81ab94214a774a61de29ed4297ff840d7cd
RajatH2007 <77881932+RajatH2007@users.noreply.github.com>
2022-04-23 18:16:42 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyDSILOnFfmr1bRGPbPhqISonFs0hyejomE",
+    authDomain: "voting-app-e93fc.firebaseapp.com",
+    databaseURL: "https://voting-app-e93fc-default-rtdb.firebaseio.com",
+    projectId: "voting-app-e93fc",
+    storageBucket: "voting-app-e93fc.appspot.com",
+    messagingSenderId: "143823142339",
+    appId: "1:143823142339:web:0180f62d1936cbe2fe5176"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
